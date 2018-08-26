f25f0ac002a3ea98baf7d5878937a34b36d9105b
Andrea Ma <andrea.ma@hotmail.co.uk>
2018-08-26 14:44:08 +0000

    second test passed

--- vending_machine.py
+++ vending_machine.py
@@ -1,9 +1,13 @@
 from byotest import *
 
 def get_change(amount):
+    if amount ==0:
         return []
+        
+    return [1]
 
 test_are_equal(get_change(0),[])
+test_are_equal(get_change(1),[1])
 
 
 print("All test passed")
