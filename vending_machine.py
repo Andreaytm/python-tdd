389bf1ed7395349ce4301f1a323e9df969aeb706
Andrea Ma <andrea.ma@hotmail.co.uk>
2018-08-26 14:45:08 +0000

    third test passed

--- vending_machine.py
+++ vending_machine.py
@@ -4,10 +4,11 @@ def get_change(amount):
     if amount ==0:
         return []
         
-    return [1]
+    return [amount]
 
 test_are_equal(get_change(0),[])
 test_are_equal(get_change(1),[1])
+test_are_equal(get_change(2),[2])
 
 
 print("All test passed")
