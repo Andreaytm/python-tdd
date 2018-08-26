9b07d48dbafef31a0174ad733581247252c26ebc
Andrea Ma <andrea.ma@hotmail.co.uk>
2018-08-26 13:28:36 +0000

    Seventh test passed

--- evens.py
+++ evens.py
@@ -7,8 +7,10 @@ def even_number_of_evens(numbers):
     for n in numbers: 
         if n % 2 == 0:
             evens += 1
-            
-    return evens %2 == 0
+    if evens == 0:
+        return False
+    else:
+        return evens %2 == 0
     
 assert even_number_of_evens([]) == False, "No numbers"
 assert even_number_of_evens([2]) == False, "One even number"
@@ -16,6 +18,6 @@ assert even_number_of_evens([2, 4]) == True, "Two even numbers"
 assert even_number_of_evens([2, 3]) == False, "Two numbers, only one even"
 assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8]) == False, "Multiple numbers, three even"
 assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8, 5, 12]) == True, "Multiple numbers, four even"
-#assert even_number_of_evens([1, 3, 9]) == False, "No even numbers"
+assert even_number_of_evens([1, 3, 9]) == False, "No even numbers"
 
 print("All tests passed!")
