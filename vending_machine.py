342708dbcf0649ff974cd7582f436c0e69c853aa
Andrea Ma <andrea.ma@hotmail.co.uk>
2018-08-26 15:49:38 +0000

    Updated code to take a dictionary and coin denominations

--- vending_machine.py
+++ vending_machine.py
@@ -1,16 +1,26 @@
 from byotest import *
 
-eur_coins = [100, 50, 20, 10, 5, 2, 1]
-usd_coins = [100, 50, 25, 10, 5, 1]
+eur_coins = {100: 20, 50: 20, 20: 20, 10: 20, 5: 20, 2: 20, 1: 20}
+usd_coins = {100: 20, 20: 20, 50: 20, 25: 20, 10: 20, 5: 20, 1: 20}
+# Create dictionary with denomination of coin key,value
+# Used {} as is an array
+# Note amount of 20 can be overridden by passin dictionary to function
+# Use smaller denominations of coin if no coin available to make up change
+# Throw error if unable to make change
 
+"""
+use sort and reverse order to keep order of dictionary items
+"""
 def get_change(amount, coins=eur_coins):
         
     change = []
-    for coin in coins:
-        while coin <= amount:
-            amount -= coin
-            change.append(coin) 
-            
+    for denomination in sorted(coins.keys(), reverse=True):
+        while denomination <= amount and coins[denomination] > 0:
+            amount -= denomination
+            coins[denomination] -= 1
+            change.append(denomination)
+    if amount !=0:
+        raise Exception("Insufficient coins to give change.")
     return change
 
 test_are_equal(get_change(0),[])
@@ -23,7 +33,10 @@ test_are_equal(get_change(50),[50])
 test_are_equal(get_change(100),[100])
 test_are_equal(get_change(3),[2, 1])
 test_are_equal(get_change(7),[5, 2])
-test_are_equal(get_change(9),[5,2,2])
-test_are_equal(get_change(35, usd_coins), [25,10])
+test_are_equal(get_change(9),[5, 2, 2])
+test_are_equal(get_change(35, usd_coins), [25, 10])
+test_are_equal(get_change(35), [20, 10, 5])
+test_are_equal(get_change(5, {2:1, 1:3}), [2, 1, 1, 1]) #1*2p & 4*1p
+test_are_equal(get_change(5, {2: 1, 1: 2}), "Insufficient coins to give change.")
 
 print("All tests passed")
